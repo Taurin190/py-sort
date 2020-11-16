b35fe45fab0ff69171418263f88cb0b722ccd03e
Koichi Taura <taura.koichi@gmail.com>
2020-11-17 08:59:28 +0900

    add insert sort

---
+++ insert_sort.py
@@ -0,0 +1,17 @@
+numbers = input().split()
+
+length = len(numbers)
+
+sorted_numbers = []
+
+for i in range(length):
+    if len(sorted_numbers) == 0:
+        sorted_numbers.append(numbers[i])
+        continue
+    for j in range(len(sorted_numbers)):
+        if int(numbers[i]) < int(sorted_numbers[j]):
+            sorted_numbers.insert(j, numbers[i])
+            break
+        if j == len(sorted_numbers) - 1:
+            sorted_numbers.append(numbers[i])
+print(sorted_numbers)
